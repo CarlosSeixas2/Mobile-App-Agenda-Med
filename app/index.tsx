f08217f2b1d222c1309b15b0f2f51e862fe25d92
Seixas <carlosseixasof@gmail.com>
2025-05-05 09:40:21 -0300

    Primeiro Commit

---
+++ app/index.tsx
@@ -0,0 +1,26 @@
+import BottomTabNavigator from "@/components/BottomTabNavigator";
+import { Link } from "expo-router";
+import { TouchableOpacity, View, Text } from "react-native";
+
+export default function App() {
+  // return <BottomTabNavigator />;
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Link href="/login" asChild>
+        <TouchableOpacity
+          style={{ padding: 10, backgroundColor: "#007AFF", borderRadius: 5 }}
+        >
+          <Text style={{ color: "white" }}>Go to Login</Text>
+        </TouchableOpacity>
+      </Link>
+
+      <Link href="/register" asChild>
+        <TouchableOpacity
+          style={{ padding: 10, backgroundColor: "#007AFF", borderRadius: 5 }}
+        >
+          <Text style={{ color: "white" }}>Go to Register</Text>
+        </TouchableOpacity>
+      </Link>
+    </View>
+  );
+}
